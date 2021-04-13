c54a6f1a9918b42fd623f29b712f2e2c247487da
Mads Bo Mikkelsen <au615041@uni.au.dk>
2021-04-13 20:36:08 +0200

    Added ExternalServer

---
+++ ExternalServer/ExternalServer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace ExternalServer
+{
+    class ExternalServer
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                // create Exchange
+                channel.QueueDeclare(
+                    queue: "reservation",
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+
+                var message = GetMessage(args);
+
+                var body = Encoding.UTF8.GetBytes(message);
+
+                // var props = channel.CreateBasicProperties();
+
+                channel.BasicPublish(
+                    exchange: "",
+                    routingKey: "reservation",
+                    basicProperties: null,
+                    body);
+
+                Console.WriteLine(" [x] sent {0}", message);
+            }
+
+            Console.WriteLine("Pres [enter] to exit.");
+            Console.ReadLine();
+
+        }
+
+        private static string GetMessage(string[] args)
+        {
+            return ((args.Length > 0) ? string.Join(" ", args) : " default should not be here");
+        }
+    }
+}
---
+++ ExternalServer/README.md
@@ -0,0 +1,2 @@
+For at køre denne ExternalServer åben en terminal og indtast
+dotnet run "Rabbit Hotel room 2"
