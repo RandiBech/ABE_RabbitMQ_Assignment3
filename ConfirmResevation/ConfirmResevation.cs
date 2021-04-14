904698b13237327970b63b840db5ae7717de74e4
Mads Bo Mikkelsen <au615041@uni.au.dk>
2021-04-14 21:36:29 +0200

    added a queue for confirms - not finished

--- ConfirmResevation/ConfirmResevation.cs
+++ ConfirmResevation/ConfirmResevation.cs
@@ -1,12 +1,45 @@
 ﻿using System;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using System.Threading;
 
 namespace ConfirmResevation
 {
-    class Program
+    class ConfirmResevation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(
+                    queue: "confirms",
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+
+                Console.WriteLine(" [*] Awaiting reservation confirmation");
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (sender, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(" [x] Received Reservation confirmation. Resevation at {0} confirmed", message);
+                };
+            }
+
+            channel.BasicConsume(
+                queue: "confirms",
+                autoAck: true,
+                consumer: consumer);
+
+            Console.WriteLine("Press [enter] to exit.");
+            Console.ReadLine();
+
         }
     }
 }
