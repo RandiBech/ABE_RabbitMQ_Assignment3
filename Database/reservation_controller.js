eca2f7c3cc59d1020c484940fa6475b9caa73004
Randi Mikkelsen Bech <au615785@uni.au.dk>
2021-04-17 09:13:36 +0200

    Add simple method in reservation controller in database

--- Database/reservation_controller.js
+++ Database/reservation_controller.js
@@ -1,6 +1,18 @@
-
+const hotelCollection = require('../models/hotel');
 
 //metode til håndtering af request om reservation
 //-- undersøg om værelse er ledigt
 //-- hvis ja, send confirm til ConfirmReservation og ændre db med nu er værelse optaget
 //-- hvis nej, send reservation not available til ConfirmReservation
+
+module.exports.handleReservationRequest = async function (req, res) {
+    console.log('Reservation controller is called');
+    // const hotelName = req.hotelName;
+    // const rommNo = req.rommNo;
+    // try {
+    //     let hotel = await hotelCollection
+
+    // } catch (error) {
+        
+    // }
+}
