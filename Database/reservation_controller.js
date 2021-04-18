e259aa87669edfe5336026f4c7ad4e8c62adfa02
Mads Bo Mikkelsen <au615041@uni.au.dk>
2021-04-18 08:40:43 +0200

    Add sendConfirmation

--- Database/reservation_controller.js
+++ Database/reservation_controller.js
@@ -1,5 +1,6 @@
 require('./db');
 var hotelCollection = require('./models/hotel');
+var confirmationQueue = require('./sendConfirmation');
 
 //metode til håndtering af request om reservation
 //-- undersøg om værelse er ledigt
@@ -7,46 +8,52 @@ var hotelCollection = require('./models/hotel');
 //-- hvis nej, send reservation not available til ConfirmReservation
 
 module.exports.handleReservationRequest = async function (req, res) {
-    console.log('Reservation controller is called');
-    const hotelName = req.hotelName;
-    const roomNumber = parseInt(req.roomNo);
-    try {
-        let hotel = await hotelCollection.findOne({ name: hotelName });
-        let room = hotel.rooms.find(function (room) {
-            if(room.roomNo === roomNumber) return true;
-        } );
-        if (room.reservation) {
-            console.log('The room is not available');
-            //TODO: ved ikke om vi skal sende statuskoder?
-            // res.status(201).json({
-            //     "title": "Room not available"
-            // })
-        } else {
-            console.log('The room is free to reservate');
-            //TODO: Kan ikke få fat på id'et!!!
-            const updatedRoom = await hotelCollection.findByIdAndUpdate(room._id, {
-                reservation: true
-            }, {
-                new: true
-            });
-            if(updatedRoom){
-                //TODO: send confirmation til ConfirmReservation
-                 return console.log('Succesfully booked');
-                }
-            else {
-                //TODO: send confirmation til ConfirmReservation
+	console.log('Reservation controller is called');
+	const hotelName = req.hotelName;
+	const roomNumber = parseInt(req.roomNo);
+	try {
+		let hotel = await hotelCollection.findOne({ name: hotelName });
+		let room = hotel.rooms.find(function (room) {
+			if (room.roomNo === roomNumber) return true;
+		});
+		if (room.reservation) {
+			console.log('The room is not available');
+			//TODO: ved ikke om vi skal sende statuskoder?
+			// res.status(201).json({
+			//     "title": "Room not available"
+			// })
+		} else {
+			console.log('The room is free to reservate');
+			//TODO: Kan ikke få fat på id'et!!!
+			const updatedRoom = await hotelCollection.findByIdAndUpdate(
+				room._id,
+				{
+					reservation: true,
+				},
+				{
+					new: true,
+				}
+			);
+			if (updatedRoom) {
+				//TODO: send confirmation til ConfirmReservation
+				var message = '';
+				confirmationQueue.sendConfirmation(message);
+				return console.log('Succesfully booked');
+			} else {
+				//TODO: send confirmation til ConfirmReservation
+				var message = '';
+				confirmationQueue.sendConfirmation(message);
 
-                console.log('unknown server error');
-            }
-        }
+				console.log('unknown server error');
+			}
+		}
+	} catch (error) {
+		console.log(error);
+		//TODO: ved ikke om vi skal sende statuskoder?
 
-    } catch (error) {
-        console.log(error);
-            //TODO: ved ikke om vi skal sende statuskoder?
-
-        // res.status(400).json({
-        //     "title": "Unable to connect to database",
-        //     "detail": error
-        // })
-    }
-}
+		// res.status(400).json({
+		//     "title": "Unable to connect to database",
+		//     "detail": error
+		// })
+	}
+};
--- Database/sendConfirmation.js
+++ Database/sendConfirmation.js
@@ -2,7 +2,7 @@ var amqp = require('amqplib/callback_api');
 
 module.exports = { sendConfirmation };
 
-async function sendConfirmation(msg, mq) {
+async function sendConfirmation(msg) {
 	amqp.connect('amqp://localhost', function (error0, connection) {
 		if (error0) {
 			throw error0;
@@ -10,16 +10,17 @@ async function sendConfirmation(msg, mq) {
 
 		connection.createChannel(function (error1, channel) {
 			if (error1) throw error1;
+
 			var queue = 'confirmation';
 
-			// get message to send
+			var message = msg;
 
 			channel.assertQueue(queue, {
 				durable: false,
 			});
 
-			channel.sendToQueue(queue, Buffer.from(msg));
-			console.log('Sent %s', msg);
+			channel.sendToQueue(queue, Buffer.from(message));
+			console.log('Sent %s', message);
 		});
 		setTimeout(function () {
 			connection.close();
